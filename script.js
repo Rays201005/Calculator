e952eda26ec73d2925909ad25e0c8564de0aaa56
Rays201005 <rays201005monday@gmail.com>
2024-05-20 12:46:36 +0700

    小數點修復

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ document.getElementById('profitLossForm').addEventListener('submit', function(ev
     const direction = document.getElementById('direction').value;
 
     const { pnl, breakevenPrice } = calculateTradeInfo(coinQuantity, entryPrice, exitPrice, direction);
-    document.getElementById('profitLossResult').textContent = `盈虧為: $${pnl.toFixed(4)}, 損益平衡價格: ${breakevenPrice.toFixed(4)}`;
+    document.getElementById('profitLossResult').textContent = `盈虧: $${pnl.toFixed(2)} , 損益平衡價格: ${breakevenPrice.toFixed(7)}`;
 });
 
 // 加倉成本計算邏輯
@@ -43,5 +43,5 @@ document.getElementById('averageCostForm').addEventListener('submit', function(e
     const purchasePrice = parseFloat(document.getElementById('purchase_price').value);
 
     const newAveragePrice = calculateNewAveragePrice(currentEthQuantity, currentAveragePrice, additionalEthQuantity, purchasePrice);
-    document.getElementById('averageCostResult').textContent = `損益平衡均價: ${newAveragePrice.toFixed(4)}`;
+    document.getElementById('averageCostResult').textContent = `損益平衡均價: ${newAveragePrice.toFixed(7)}`;
 });
